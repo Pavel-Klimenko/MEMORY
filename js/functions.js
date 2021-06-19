515445afba10d9451ef4d902c6261846c4710931
PavelKlimenko <p.klimenko@techin.by>
2021-06-19 15:23:04 +0300

    Have modified code style. Fixed bug with opened single card

--- js/functions.js
+++ js/functions.js
@@ -1,53 +1,53 @@
 function openAllcards() {
-    $('img').each(function(){
-       var cardName = $(this).attr('class').split(' ')[0];
-       if (cardName != 'freezed') {
-          $(this).attr("src", "img/"+cardName+".jpg");
-       }
-   });  
- }
- 
- function closeAllcars() {
+    $("img").each(function() {
+        var cardName = $(this).attr("class").split(" ")[0];
+        if (cardName != "freezed") {
+            $(this).attr("src", "img/" + cardName + ".jpg");
+        }
+    });
+}
+
+function closeAllcars() {
     $("img").attr("src", "img/back.jpg");
- }
- 
- function makeRandomArr(a, b) {
+}
+
+function makeRandomArr(a, b) {
     return Math.random() - 0.5;
- }
- 
- function findCardPositions(arrCards, cardName) {
+}
+
+function findCardPositions(arrCards, cardName) {
     let index1 = arrCards.findIndex(rank => rank === cardName);
     let index2 = arrCards.findIndex(
-       (rank, index) => rank === cardName && index > index1
+        (rank, index) => rank === cardName && index > index1
     );
- 
+
     return [index1, index2];
- }
- 
- function shuffleСards() {
+}
+
+function shuffleСards() {
     arrCards.sort(makeRandomArr);
-    assignPairCards('joker');
-    assignPairCards('ace');
-    assignPairCards('king');
-    assignPairCards('queen');
-    assignPairCards('jack');
+    assignPairCards("joker");
+    assignPairCards("ace");
+    assignPairCards("king");
+    assignPairCards("queen");
+    assignPairCards("jack");
     return true;
- }
- 
- function assignPairCards(cardName) {
+}
+
+function assignPairCards(cardName) {
     var positions = findCardPositions(arrCards, cardName);
     assignCardClass(positions[0], cardName);
     assignCardClass(positions[1], cardName);
- }
- 
- function assignCardClass(position, cardName) {
-        $("img:eq("+position+")").attr('class', cardName);
- }
+}
 
- function openModal(modalId) {
-   location.href = modalId;
+function assignCardClass(position, cardName) {
+    $("img:eq(" + position + ")").attr("class", cardName);
+}
 
-   setTimeout(function() {
-      location.href = "#close";
-   }, 1000);
+function openModal(modalId) {
+    location.href = modalId;
+
+    setTimeout(function() {
+        location.href = "#close";
+    }, 1000);
 }
--- js/script.js
+++ js/script.js
@@ -1,89 +1,95 @@
 //объявление массива с классами разных имен карт
-let arrCards = ['joker','joker','ace','ace','king','king','queen','queen', 'jack','jack'];
+let arrCards = ["joker", "joker", "ace", "ace", "king", "king", "queen", "queen", "jack", "jack"];
 
 $(document).ready(function() {
 
-      if (shuffleСards()) {
-         openAllcards(); 
-      }
+    if (shuffleСards()) {
+        openAllcards();
+    }
 
-      var gameStatus = 'waiting';
-      // waiting - ожидани начала игры 
-      // Флаг статуса игры:
-      // playing - идет игра,
-      // finished - игра завершена,
-      // victory - победа
+    var gameStatus = "waiting";
+    // waiting - ожидани начала игры 
+    // Флаг статуса игры:
+    // playing - идет игра,
+    // finished - игра завершена,
+    // victory - победа
 
-      $("#start-finish").click(function() {
-         //Старт игры
-         if (gameStatus != undefined && gameStatus != 'playing') {
+    $("#start-finish").click(function() {
+        //Старт игры
+        if (gameStatus != undefined && gameStatus != "playing") {
             closeAllcars();
             shuffleСards();
-            gameStatus = 'playing';
+            gameStatus = "playing";
             openModal("#start");
             $("#start-finish").html("FINISH");
             return false;
-         }
+        }
 
-         //Завершение игры по требованию пользователя
-         if (gameStatus == 'playing') {
+        //Завершение игры по требованию пользователя
+        if (gameStatus == "playing") {
             openAllcards();
-            gameStatus = 'finished';
+            gameStatus = "finished";
             openModal("#finish");
             $("#start-finish").html("START");
             return false;
-         }
-      });
+        }
+    });
 
-      var arrOpenedCards = [];
+    var arrOpenedCards = [];
 
-        $("img").click(function openCard() {
+    $("img").click(function openCard() {
 
-            //выходим из функции если игра не запущена
-            if (gameStatus != "playing") {
-                return true;
+        //выходим из функции если игра не запущена
+        if (gameStatus != "playing") {
+            return true;
+        }
+
+        //запрет клика на карту из успешно найденной пары карт
+        if (cardName == "freezed") {
+            return true;
+        }
+
+        //запрет открытия более 2-х карт одновременно
+        if ($(".opened").length >= 2) {
+            return true;
+        }
+
+        //запрет нажатия на 
+        if ($(this).hasClass(".opened")) {
+            return false;
+        }
+
+
+        //выходим из функции если успешно найдены все пары карт и устанавливаем gameStatus = "victory";
+        if ($(".freezed").length >= 8) {
+            gameStatus = "victory";
+            openAllcards();
+            $("#start-finish").html("PLAY AGAIN");
+            openModal("#win");
+            return true;
+        }
+
+        var cardName = $(this).attr("class");
+
+        $(this).attr("src", "img/" + cardName + ".jpg").addClass("opened");
+        arrOpenedCards.push(cardName);
+
+        if (arrOpenedCards.length >= 2) {
+
+            if (arrOpenedCards[0] == arrOpenedCards[1]) {
+                $("img.opened").attr("class", "freezed");
+                arrOpenedCards = [];
+                return false;
+            } else {
+
+                setTimeout(function() {
+                    if (gameStatus != "finished") {
+                        $("img.opened").attr("src", "img/back.jpg").removeClass("opened");
+                    }
+                }, 2000);
+                arrOpenedCards = [];
+                return false;
             }
-
-            //запрет клика на карту из успешно найденной пары карт
-            if (cardName == "freezed") {
-                return true;
-            }
-
-            //запрет открытия более 2-х карт одновременно
-            if ($(".opened").length >= 2) {
-               return true;
-            }
-
-            //выходим из функции если успешно найдены все пары карт и устанавливаем gameStatus = 'victory';
-            if ($(".freezed").length >= 8) {
-               gameStatus = 'victory';
-               openAllcards();
-               $("#start-finish").html("PLAY AGAIN");
-               openModal("#win");
-               return true;
-            }
-
-            var cardName = $(this).attr("class");
-
-            $(this).attr("src", "img/"+cardName+".jpg").addClass("opened");
-            arrOpenedCards.push(cardName);
-
-            if (arrOpenedCards.length >= 2) {
-
-                    if (arrOpenedCards[0] == arrOpenedCards[1]) {
-                        $("img.opened").attr("class", "freezed");
-                        arrOpenedCards = [];
-                        return false;
-                    } else {
-
-                        setTimeout(function() {
-                           if (gameStatus != 'finished') {
-                              $("img.opened").attr("src", "img/back.jpg").removeClass("opened");
-                           }
-                        }, 2000);
-                        arrOpenedCards = [];
-                        return false;
-                    }     
-            }
+        }
     });
 });
